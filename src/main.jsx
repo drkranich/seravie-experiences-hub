9053dd307afac698d69e75f4e6e6e7964d933841
drkranich <147064193+drkranich@users.noreply.github.com>
2026-07-30 21:39:14 -0300

    feat: phase2 - wrap App with TenantProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { TenantProvider } from './hooks/useTenant'
 import App from './App'
-import { LanguageProvider } from './i18n/LanguageContext'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LanguageProvider>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <TenantProvider>
       <App />
-    </LanguageProvider>
-  </React.StrictMode>,
+    </TenantProvider>
+  </StrictMode>
 )
